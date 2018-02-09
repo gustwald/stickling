d4d1f19f74132ab078c3d596bb9b21c411b88746
Liam Öberg <obergliam@gmail.com>
2018-02-09 01:48:42 +0100

    some design added on navigation

--- src/js/components/Navigation/Navigation.js
+++ src/js/components/Navigation/Navigation.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router';
 import styles from './Navigation.scss';
+import logo from './../../../../assets/logo.js';
+
+console.log(logo);
 
 const Navigation = () => (
-  <div>
-    <Link to="/">Hem</Link>
-    <Link to="/profile">Profil</Link>
-    <Link to="/ads">Annonser</Link>
+  <div className={styles.navBar}>
+    <div className={styles.logo}></div>
+    <Link className={styles.links} to="/">Hem</Link>
+    <Link className={styles.links} to="/profile">Profil</Link>
+    <Link className={styles.links} to="/ads">Annonser</Link>
   </div>
 );
 
