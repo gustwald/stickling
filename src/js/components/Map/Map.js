944b4f26dabe003380ed60aa8c94bd76230551d7
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-02-12 19:21:40 +0100

    fixed freightcost not showing and adding a map

--- src/js/components/Map/Map.js
+++ src/js/components/Map/Map.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import styles from './Map.scss';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-const Map = () => (
-  <div className="container">
-    <h2>karta</h2>
-  </div>
+const Map = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: 59.334591, lng: 18.06324 }}
+      defaultOptions={{
+        // these following 7 options turn certain controls off see link below
+        streetViewControl: false,
+        scaleControl: false,
+        mapTypeControl: false,
+        panControl: false,
+        // zoomControl: false,
+        rotateControl: false,
+        fullscreenControl: false
+      }}
+    >
+      {props.isMarkerShown && <Marker position={{ lat: 59.575548, lng: 18.500977 }} />}
+    </GoogleMap>
+  ))
 );
 
 export default Map;
--- src/js/pages/MapPage.js
+++ src/js/pages/MapPage.js
@@ -4,7 +4,13 @@ import Map from '../components/Map/Map';
 const MapPage = () => (
   <div>
     <h1>Karta</h1>
-    <Map />
+    <Map
+      isMarkerShown
+      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `400px` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
   </div>
 );
 
