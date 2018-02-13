7934e2a2017311d53e2424bb857c43d0d966544c
Liam Öberg <obergliam@gmail.com>
2018-02-13 18:14:55 +0100

    Small fixes, nothing to report

--- src/js/components/App/App.js
+++ src/js/components/App/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styles from './App.scss';
-// import GoogleSignup from '../GoogleSignup/GoogleSignup';
-// import SignOut from '../SignOut/SignOut';
-// import CurrentUser from '../CurrentUser/CurrentUser';
-// import { getCurrentUser } from '../../Selector';
-// import StandardSignup from '../StandardSignup/StandardSignup';
-// import StandardSignin from '../StandardSingIn/StandardSignin';
+import CurrentUser from '../CurrentUser/CurrentUser';
+import { getCurrentUser } from '../../Selector';
 
 class App extends Component {
-  componentDidMount() { }
+  componentDidMount() {}
   render() {
     return (
-      <div className="container">
-        <h1 className={styles.heading}>stickling</h1>
+      <div className={styles.container}>
         {/* <CurrentUser />
         {this.props.currentUser
           ? null
--- src/js/pages/HomePage.js
+++ src/js/pages/HomePage.js
@@ -4,6 +4,7 @@ import CurrentUser from '../components/CurrentUser/CurrentUser';
 import { getCurrentUser } from '../Selector';
 import LoginModal from '../components/LoginModal/LoginModal';
 import HomePageAds from '../components/HomePageAds/HomePageAds';
+import Navigation from '../components/Navigation/Navigation';
 import SignOut from '../components/SignOut/SignOut';
 import styles from './HomePage.scss';
 
@@ -11,9 +12,8 @@ class HomePage extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <HomePageAds />
-
         <CurrentUser />
+        <HomePageAds />
         {this.props.currentUser ? null : <LoginModal />}
         {this.props.currentUser ? <SignOut /> : null}
       </div>
