fe07cb2baab72fc8592f0ad9f9480c7690ce563f
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-02-13 10:51:35 +0100

    merge

--- src/js/components/Map/Map.js
+++ src/js/components/Map/Map.js
@@ -1,10 +1,147 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
+const mapStyle = [
+  {
+    featureType: 'landscape.man_made',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f7f1df'
+      }
+    ]
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#d0e3b4'
+      }
+    ]
+  },
+  {
+    featureType: 'landscape.natural.terrain',
+    elementType: 'geometry',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'poi.business',
+    elementType: 'all',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'poi.medical',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#fbd3da'
+      }
+    ]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#bde6ab'
+      }
+    ]
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#ffe15f'
+      }
+    ]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#efd151'
+      }
+    ]
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#ffffff'
+      }
+    ]
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: 'black'
+      }
+    ]
+  },
+  {
+    featureType: 'transit.station.airport',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#cfb2db'
+      }
+    ]
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#a2daf2'
+      }
+    ]
+  }
+];
 const Map = withScriptjs(
   withGoogleMap(props => (
     <GoogleMap
-      defaultZoom={8}
+      defaultZoom={11}
       defaultCenter={{ lat: 59.334591, lng: 18.06324 }}
       defaultOptions={{
         // these following 7 options turn certain controls off see link below
@@ -14,10 +151,11 @@ const Map = withScriptjs(
         panControl: false,
         // zoomControl: false,
         rotateControl: false,
-        fullscreenControl: false
+        fullscreenControl: false,
+        styles: mapStyle
       }}
     >
-      {props.isMarkerShown && <Marker position={{ lat: 59.575548, lng: 18.500977 }} />}
+      {props.isMarkerShown && <Marker position={{ lat: 59.276275, lng: 18.067017 }} />}
     </GoogleMap>
   ))
 );
