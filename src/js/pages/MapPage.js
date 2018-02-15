f56cffc412f99034bce1cbb18933415986b2d6ba
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-02-15 12:55:18 +0100

    slidein and slideout when clicking on ad

--- src/js/pages/MapPage.js
+++ src/js/pages/MapPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Map from '../components/Map/Map';
+import styles from './MapPage.scss';
 
 class MapPage extends React.PureComponent {
   state = {
     isMarkerShown: true,
-    mapWidth: '100%'
+    mapWidth: '100%',
+    ad: {}
   };
 
   // componentDidMount() {
@@ -19,19 +21,39 @@ class MapPage extends React.PureComponent {
 
   handleMarkerClick = ad => {
     console.log(ad);
-    this.setState({ mapWidth: '70%' });
+    this.setState({ mapWidth: '60%', ad });
+  };
+
+  closeMarkerClick = () => {
+    console.log('stänger');
+    this.setState({ ad: {}, mapWidth: '100%' });
   };
 
   render() {
     return (
-      <Map
-        isMarkerShown={this.state.isMarkerShown}
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px`, width: this.state.mapWidth }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-        onMarkerClick={this.handleMarkerClick}
-      />
+      <div>
+        <Map
+          isMarkerShown={this.state.isMarkerShown}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={
+            <div
+              className={styles.container}
+              style={{ height: `400px`, width: this.state.mapWidth }}
+            />
+          }
+          mapElement={<div style={{ height: `100%` }} />}
+          onMarkerClick={this.handleMarkerClick}
+        />
+        {this.state.mapWidth === '100%' ? null : (
+          <div>
+            <button onClick={this.closeMarkerClick}>stäng</button>
+            <h1>{this.state.ad.adTitle}</h1>
+            <p>{this.state.ad.adText}</p>
+            <p>{this.state.ad.adPrice}</p>
+          </div>
+        )}
+      </div>
     );
   }
 }
