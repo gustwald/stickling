4348ab334ba75d2498447e96237da08b59a87792
Liam Öberg <obergliam@gmail.com>
2018-02-15 17:01:43 +0100

    Changed so profile is in the correct place in the filestructure

--- src/js/components/Profile/Profile.js
+++ src/js/components/Profile/Profile.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CurrentUser from '../CurrentUser/CurrentUser';
+import { getCurrentUser } from '../../Selector';
 import styles from './Profile.scss';
+import herotest from '../../../../assets/hero-test.jpg';
+import gurt from '../../../../assets/gurt.png';
 
-const Profile = () => (
-  <div className="container">
-    <h1>profile</h1>
-  </div>
-);
+class Profile extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.hero} style={{ backgroundImage: `url(${herotest})` }} />
+        <div className={styles.mainContent}>
+          <div className={styles.profilePic} style={{ backgroundImage: `url(${gurt})` }} />
+          <div className={styles.profileInfo}>
+            <h1>{this.props.currentUser.first + ' ' + this.props.currentUser.last}</h1>
+            <h3>{this.props.currentUser.email}</h3>
+          </div>
+          <div className={styles.adContainer}>
+            <div className={styles.ad} />
+            <div className={styles.ad} />
+            <div className={styles.ad} />
+            <div className={styles.ad} />
+            <div className={styles.ad} />
+            <div className={styles.ad} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-export default Profile;
+const mapStateToProps = state => ({
+  currentUser: getCurrentUser(state)
+});
+
+export default connect(mapStateToProps)(Profile);
