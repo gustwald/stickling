a9465bc6adeeb47e68ea0d8a1fccda232ea2ae81
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-02-20 12:49:52 +0100

    navigation fix

--- src/js/components/Navigation/Navigation.js
+++ src/js/components/Navigation/Navigation.js
@@ -4,47 +4,40 @@ import styles from './Navigation.scss';
 import logo from './../../../../assets/logo.svg';
 import { CSSTransitionGroup } from 'react-transition-group';
 
-// const Navigation = () => (
-//   <div className={styles.navBar}>
-//     <div style={{ backgroundImage: `url(${logo})` }} className={styles.logo} />
-//     <CSSTransitionGroup 
-//               transitionName="example"
-//               transitionAppear={true}
-//               transitionAppearTimeout={500}
-//     >
-
-//     <Link className={styles.links} to="/">
-//       Hem
-//     </Link>
-//     <Link className={styles.links} to="/profil">
-//       Profil
-//     </Link>
-//     <Link className={styles.links} to="/annonser">
-//       Annonser
-//     </Link>
-//     <Link className={styles.links} to="/karta">
-//       karta
-//     </Link>
-//     </CSSTransitionGroup>
-//   </div>
-// );
-
 const Navigation = () => (
-  <CSSTransitionGroup 
-    component="ul"
-    className={styles.navBar}
-    transitionName="example"
-    transitionAppear={true}
-    transitionAppearTimeout={3000}
-    transitionEnterTimeout={3000}
-    transitionLeaveTimeout={3000}
-    >
-    <li style={{ backgroundImage: `url(${logo})` }} className={styles.logo} />
-    <li className={styles.links} to="/">Hem</li>
-    <li className={styles.links} to="/profil">Profil</li>
-    <li className={styles.links} to="/annonser">Annonser</li>
-    <li className={styles.links} to="/karta">Karta</li>
-  </CSSTransitionGroup>
+  <div className={styles.navBar}>
+    <div style={{ backgroundImage: `url(${logo})` }} className={styles.logo} />
+    <Link className={styles.links} to="/">
+      Hem
+    </Link>
+    <Link className={styles.links} to="/profil">
+      Profil
+    </Link>
+    <Link className={styles.links} to="/annonser">
+      Annonser
+    </Link>
+    <Link className={styles.links} to="/karta">
+      karta
+    </Link>
+  </div>
 );
+
+// const Navigation = () => (
+//   <CSSTransitionGroup
+//     component="ul"
+//     className={styles.navBar}
+//     transitionName="example"
+//     transitionAppear={true}
+//     transitionAppearTimeout={3000}
+//     transitionEnterTimeout={3000}
+//     transitionLeaveTimeout={3000}
+//     >
+//     <li style={{ backgroundImage: `url(${logo})` }} className={styles.logo} />
+//     <li className={styles.links} to="/">Hem</li>
+//     <li className={styles.links} to="/profil">Profil</li>
+//     <li className={styles.links} to="/annonser">Annonser</li>
+//     <li className={styles.links} to="/karta">Karta</li>
+//   </CSSTransitionGroup>
+// );
 
 export default Navigation;
