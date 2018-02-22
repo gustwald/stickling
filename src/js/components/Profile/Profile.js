52f75ad91940d6e658709f30563be00d2badd7fd
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-02-22 14:29:52 +0100

    cleanup profilepage

--- src/js/components/Profile/Profile.js
+++ src/js/components/Profile/Profile.js
@@ -3,29 +3,15 @@ import { connect } from 'react-redux';
 import CurrentUser from '../CurrentUser/CurrentUser';
 import { getCurrentUser } from '../../Selector';
 import styles from './Profile.scss';
-import herotest from '../../../../assets/hero-test.jpg';
 import gurt from '../../../../assets/gurt.png';
 
 class Profile extends Component {
+  state = {};
   render() {
     return (
       <div className={styles.container}>
-        <div className={styles.hero} style={{ backgroundImage: `url(${herotest})` }} />
-        <div className={styles.mainContent}>
-          <div className={styles.profilePic} style={{ backgroundImage: `url(${gurt})` }} />
-          <div className={styles.profileInfo}>
-            <h1>{this.props.currentUser.first + ' ' + this.props.currentUser.last}</h1>
-            <h3>{this.props.currentUser.email}</h3>
-          </div>
-          <div className={styles.adContainer}>
-            <div className={styles.ad} />
-            <div className={styles.ad} />
-            <div className={styles.ad} />
-            <div className={styles.ad} />
-            <div className={styles.ad} />
-            <div className={styles.ad} />
-          </div>
-        </div>
+        {/* <h1>{this.props.currentUser.first + ' ' + this.props.currentUser.last}</h1>
+            <h3>{this.props.currentUser.email}</h3> */}
       </div>
     );
   }
