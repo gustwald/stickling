4761463265f5860486a26a698bddbcf628c5e9ba
Liam Öberg <obergliam@gmail.com>
2018-02-22 16:14:44 +0100

    Displayed ads on the profilepage and came up with some styling

--- src/js/components/Profile/Profile.js
+++ src/js/components/Profile/Profile.js
@@ -4,12 +4,60 @@ import CurrentUser from '../CurrentUser/CurrentUser';
 import { getCurrentUser } from '../../Selector';
 import styles from './Profile.scss';
 import gurt from '../../../../assets/gurt.png';
+import { Col, Row, Card, CardTitle } from 'react-materialize';
+import twitter from '../../../../assets/twitter.svg';
+import insta from '../../../../assets/instagram.svg';
 
 class Profile extends Component {
   state = {};
   render() {
     return (
-      <div className={styles.container}>
+      <div className="container">
+        <Row className={styles.profileInfoWrapper}>
+          <Col className={styles.profilePic} s={2} style={{ backgroundImage: `url(${gurt})` }} />
+          <Col className={styles.profileInfo} s={10}>
+            <h3>
+              <span>{this.props.currentUser.first + ' ' + this.props.currentUser.last}</span>
+            </h3>
+            <div className={styles.socialMedia}>
+              <div className={styles.instagram} style={{ backgroundImage: `url(${insta})` }} />
+              <div className={styles.twitter} style={{ backgroundImage: `url(${twitter})` }} />
+            </div>
+          </Col>
+        </Row>
+
+        <Row className={styles.adsWrapper}>
+          {this.props.ads.map(ad => (
+            <Col className={styles.gridItem} key={ad.id} s={12} m={6} l={4}>
+              <Card
+                className={styles.card}
+                header={
+                  <CardTitle
+                    // className={styles.cardImage}
+                    // style={{ backgroundImage: `url(${ad.image})` }}
+                    image={ad.image}
+                    reveal
+                    waves="light"
+                  />
+                }
+                title={ad.adTitle}
+                reveal={
+                  <div>
+                    <p>{ad.adText}</p>
+                    <p>{ad.adPrice + 'kr'}</p>
+                    {ad.adShips ? <p>Kan skickas</p> : <p>Skickas inte</p>}
+                    {ad.adPickup ? <p>Kan hämtas</p> : <p>Hämtas inte</p>}
+                  </div>
+                }
+              >
+                <p>
+                  <a href="#">This is a link</a>
+                </p>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+
         {/* <h1>{this.props.currentUser.first + ' ' + this.props.currentUser.last}</h1>
             <h3>{this.props.currentUser.email}</h3> */}
       </div>
@@ -17,8 +65,14 @@ class Profile extends Component {
   }
 }
 
-const mapStateToProps = state => ({
-  currentUser: getCurrentUser(state)
-});
+const mapStateToProps = state => {
+  const userAds = state.adsReducer.filter(ad => ad.uId === state.currentUser.id);
+  console.log(userAds);
+
+  return {
+    currentUser: getCurrentUser(state),
+    ads: userAds
+  };
+};
 
 export default connect(mapStateToProps)(Profile);
