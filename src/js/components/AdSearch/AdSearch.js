1824f44033dcb634d323b6e4429dc0328ee3108f
Liam Öberg <obergliam@gmail.com>
2018-03-06 21:40:06 +0100

    made a little animation for desktop logo and added search icon for the searchfields

--- src/js/components/AdSearch/AdSearch.js
+++ src/js/components/AdSearch/AdSearch.js
@@ -4,13 +4,15 @@ import styles from './AdSearch.scss';
 
 const AdSearch = ({ ads, onSearch }) => (
   <div className={styles.container}>
-    <input
-      className={styles.searchInput}
-      placeholder="Sök"
-      onChange={e => onSearch(e.target.value)}
-      style={{ width: 200 }}
-      type="text"
-    />
+    <span>
+      <input
+        className={styles.searchInput}
+        placeholder="Sök"
+        onChange={e => onSearch(e.target.value)}
+        style={{ width: 200 }}
+        type="text"
+      />
+    </span>
   </div>
 );
 
