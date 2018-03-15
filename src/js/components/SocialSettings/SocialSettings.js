492d24a6559d82475c715f70f9e5fb0fb20d734a
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-03-16 00:21:38 +0100

    added modal for socialsettings button

--- src/js/components/SocialSettings/SocialSettings.js
+++ src/js/components/SocialSettings/SocialSettings.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
-import { Icon } from 'antd';
+import { Icon, Modal } from 'antd';
 import styles from './SocialSettings.scss';
 
 class SocialSettings extends Component {
+  state = {
+    socialModal: false
+  };
+
+  addSocialInformation = () => {
+    this.setState({
+      socialModal: true
+    });
+  };
+
+  handleOk = () => {
+    this.setState({
+      socialModal: false
+    });
+  };
+
   render() {
     return (
       <div className={styles.container}>
-        <Icon className={styles.icon} type="setting" />
+        <Icon className={styles.icon} type="setting" onClick={this.addSocialInformation} />
+        <Modal
+          footer={null}
+          visible={this.state.socialModal}
+          onOk={this.handleOk}
+          onCancel={this.handleOk}
+        >
+          <form className={styles.socialForm}>
+            <label htmlFor="instagram">
+              <input
+                type="text"
+                name="instagram"
+                id="instagram"
+                placeholder="instagram"
+                onChange={this.onChange}
+              />
+            </label>
+            <label htmlFor="twitter">
+              <input
+                type="text"
+                name="twitter"
+                id="twitter"
+                placeholder="twitter"
+                onChange={this.onChange}
+              />
+            </label>
+
+            <button type="button" onClick={this.register}>
+              Registrera
+            </button>
+          </form>
+        </Modal>
       </div>
     );
   }
